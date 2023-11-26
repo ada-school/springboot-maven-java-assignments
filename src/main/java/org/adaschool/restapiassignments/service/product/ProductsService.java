ebd4a115b72c87c1a5f549d97f35af435352180f
sancarbar <sancabar@gmail.com>
2023-11-26 07:55:47 -0500

    Migrated code from original repo.

---
+++ src/main/java/org/adaschool/restapiassignments/service/product/ProductsService.java
@@ -0,0 +1,20 @@
+package org.adaschool.restapiassignments.service.product;
+
+
+import org.adaschool.restapiassignments.repository.product.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductsService {
+
+    Product save(Product product);
+
+    Optional<Product> findById(String id);
+
+    List<Product> all();
+
+    void deleteById(String id);
+
+    Product update(Product Product, String ProductId);
+}
---
+++ src/main/java/org/adaschool/restapiassignments/service/user/UsersService.java
@@ -0,0 +1,20 @@
+package org.adaschool.restapiassignments.service.user;
+
+
+import org.adaschool.restapiassignments.repository.user.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UsersService {
+
+    User save(User user);
+
+    Optional<User> findById(String id);
+
+    List<User> all();
+
+    void deleteById(String id);
+
+    User update(User user, String userId);
+}
